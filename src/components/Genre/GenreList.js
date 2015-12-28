7587667cbc4c1ce6c3c5f17f4509bbe1631f6432
vidaaudrey <audrey@33road.com>
2015-12-27 17:02:06 -0800

    feat: (genre) add basic data mockup, next step is getting real data

--- src/components/Genre/GenreList.js
+++ src/components/Genre/GenreList.js
@@ -1,12 +1,17 @@
 import React, {Component} from 'react'
+import GenreListItem from './GenreListItem'
 
 export default () => {
     return (
-        <div className="row">
-            <div className="container" id="genre-list">
-                <div className="links">
-                    <a href="genre/genre-list.html" title="">Genre List Page </a>
-                </div>
+        <div className="row text-center">
+            <h4>Movie Genre</h4>
+            <div className="container " id="genre-list">
+                    <GenreListItem />
+                    {
+                        [1,2,3,4,5,6,7,8].map((val, index) => (
+                            <GenreListItem key={val} />
+                            ))
+                    }
             </div>
         </div>
     )
---
+++ src/components/Genre/GenreListItem.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+export default () => {
+    return (
+       <div className="col-xs-6 col-md-4 card-small">
+           <a href="#" className="thumbnail">
+               <img src="http://lorempixel.com/400/200/" alt="action" />
+               <div className="caption text-center">
+                   <h3>Action</h3>
+               </div>
+           </a>
+       </div>
+    )
+}
